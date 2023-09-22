bee75b507bd83405a772a4077ded01890f9b6280
vongnguyen0933 <vongnguyen0933@gmail.com>
2023-09-22 22:43:30 +0700

    Changes to my-project noted

---
+++ Workspace/back-end/Strapi-demo/strapi-demo/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
